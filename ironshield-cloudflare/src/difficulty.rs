3b505dd4e1b3175416681665fbf13f5e11d011bd
ebosspc <software2070@protonmail.com>
2025-06-19 01:55:52 -0400

    documented difficulty function

--- ironshield-cloudflare/src/difficulty.rs
+++ ironshield-cloudflare/src/difficulty.rs
@@ -1,9 +1,15 @@
 //! # Difficultly Module for Challenge Handling.
 
-fn bot_score_to_difficulty(bot_score: u64) -> u64 {
+/// This function maps a request's liklihood of being a bot to a difficulty
+/// of a proof of work challenge. 
+/// Bots are given a score between 1 and 99 of how likely they are to be human.
+/// This bot score matches the industry standard used by Cloudflare
+/// in the cf.bot_management.score API call
+/// A score of 1 is the highest liklihood of being a bot,
+/// A score of 99 is the highest liklihood of being human.
+pub fn bot_score_to_difficulty(bot_score: u64, base_difficulty: u64, scaling_factor: u64) -> u64 {
     let inverted_score: u64 = 99 - bot_score;
-    let base_difficulty: u64 = 10_000;
-    let difficulty: u64 = ((inverted_score * inverted_score) * 1040) + base_difficulty;
+    let difficulty: u64 = ((inverted_score * inverted_score) * scaling_factor) + base_difficulty;
     difficulty
 }
 
@@ -13,8 +19,8 @@ mod tests {
 
     #[test]
     fn test_bot_score_to_difficulty() {
-        assert_eq!(bot_score_to_difficulty(99), 10_000);
-        assert_eq!(bot_score_to_difficulty(0), 10_203_040);
-        assert_eq!(bot_score_to_difficulty(1), 9_998_160);
+        assert_eq!(bot_score_to_difficulty(99, 10_000, 1040), 10_000);
+        assert_eq!(bot_score_to_difficulty(0, 10_000, 1040), 10_203_040);
+        assert_eq!(bot_score_to_difficulty(1, 10_000, 1040), 9_998_160);
     }
 }
